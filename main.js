ed082f2dc41b244525607ac42c181440b6a4db79
skadanka <rylynn2@gmail.com>
2023-06-04 17:27:27 +0300

    updated api

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ app.use(
 app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(express.static(path.join(__dirname, "public"))); //To serve static files such as images, CSS files, and JavaScript files
 //local:
-app.use(express.static(path.join(__dirname, "dist")));
+// app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/",function(req,res)
 { 
