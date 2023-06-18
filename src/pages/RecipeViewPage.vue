91a17621ef15b039a9874e62ec4b8d8443a1980e
skadanka <rylynn2@gmail.com>
2023-06-18 21:52:03 +0300

    Updated RecipeViewPage Logic, required to create many sub components to handle all the logic

--- src/pages/RecipeViewPage.vue
+++ src/pages/RecipeViewPage.vue
@@ -1,35 +1,46 @@
 <template>
   <div class="container">
     <div v-if="recipe">
+      <h1>{{ recipe.title }}</h1>
       <div class="recipe-header mt-3 mb-4">
-        <h1>{{ recipe.title }}</h1>
-        <img :src="recipe.image" class="center" />
-      </div>
-      <div class="recipe-body">
-        <div class="wrapper">
-          <div class="wrapped">
-            <div class="mb-3">
-              <div>Ready in {{ recipe.readyInMinutes }} minutes</div>
-              <div>Likes: {{ recipe.aggregateLikes }} likes</div>
+          <div>
+            <div><strong>Ready in </strong> <span class="span-decor">{{ recipe.readyInMinutes }} minutes 🧭 </span></div>
+            <div><strong>Likes: </strong>{{ recipe.popularity }} <span class="span-decor">likes </span> ❤</div>
+            <div><strong>Summary:</strong><br> {{ recipe.summary }}</div>
+            <div class="intolrences-container">
+              <div class="intolrences-item" v-if="recipe.glutenFree">🍞</div>
+              <div class="intolrences-item" v-if="recipe.vegan">🥚</div>
+              <div class="intolrences-item" v-if="recipe.vegetarian">🥦</div>
             </div>
-            Ingredients:
-            <ul>
-              <li
-                v-for="(r, index) in recipe.extendedIngredients"
-                :key="index + '_' + r.id"
-              >
-                {{ r.original }}
-              </li>
-            </ul>
+
           </div>
-          <div class="wrapped">
-            Instructions:
-            <ol>
-              <li v-for="s in recipe._instructions" :key="s.number">
-                {{ s.step }}
-              </li>
-            </ol>
+          <div class="img-wrap">
+            <img :src="recipe.image" class="center" />
           </div>
+    </div>
+    <div class="recipe-body">
+      <div class="wrapper">
+          Ingredients:
+          <ul>
+            <li
+              v-for="(r, index) in recipe.extendedIngredients"
+              :key="index + '_' + r.id"
+            >
+              <Ingridient 
+              :id = r.id
+              :Original="r.Original"
+              :aisle="r.aisle"
+              :amount="r.amount"
+              :image="r.image"
+              :consistency="r.consistency"
+              :measures="r.measures"
+              ></Ingridient>
+            </li>
+          </ul>
+        </div>
+        <div class="wrapped">
+          Instructions:
+          <instruction-list :instructions="recipe.instructions"></instruction-list>
         </div>
       </div>
       <!-- <pre>
@@ -43,12 +54,17 @@
 
 <script>
 
-
+import InstructionList from '../components/InstructionList.vue';
+import Ingridient from './Ingridient.vue';
 export default {
   data() {
     return {
       recipe: null
     };
+  },
+  components: {
+    InstructionList,
+    Ingridient
   },
   async created() {
     try {
@@ -58,9 +74,9 @@ export default {
       try {
         response = await this.axios.get(
           // "https://test-for-3-2.herokuapp.com/recipes/info",
-          this.$root.store.server_domain + "/recipes/info",
+          this.$root.store.server_domain + "/recipes/information",
           {
-            params: { id: this.$route.params.recipeId }
+            params: { recipeId: this.$route.params.recipeId }
           }
         );
 
@@ -73,16 +89,27 @@ export default {
       }
 
       let {
+        Preview,
         analyzedInstructions,
-        instructions,
         extendedIngredients,
-        aggregateLikes,
+        instructions,
+        userData,
+      } = response.data.recipeInfo;
+
+      let 
+      {
+        title,
         readyInMinutes,
         image,
-        title
-      } = response.data.recipe;
+        popularity,
+        vegan,
+        vegetarian,
+        glutenFree,
+        summary
+      } = Preview;
 
-      let _instructions = analyzedInstructions
+
+      let _instructions = response.data.recipeInfo.instructions
         .map((fstep) => {
           fstep.steps[0].step = fstep.name + fstep.steps[0].step;
           return fstep.steps;
@@ -94,13 +121,19 @@ export default {
         _instructions,
         analyzedInstructions,
         extendedIngredients,
-        aggregateLikes,
+        popularity,
         readyInMinutes,
         image,
-        title
+        title,
+        vegan,
+        vegetarian,
+        glutenFree,
+        summary,
+        userData
       };
 
       this.recipe = _recipe;
+      console.log(this.recipe)
     } catch (error) {
       console.log(error);
     }
@@ -109,19 +142,43 @@ export default {
 </script>
 
 <style scoped>
-.wrapper {
+body {
+  text-align: left;
+}
+.recipe-header {
   display: flex;
+  flex-direction: row-reverse;
+
+  font-size: larger;
 }
-.wrapped {
-  width: 50%;
+
+
+.recipe-header .img-wrap {
+  width: 100%;
+  height: 100%;
+
+  grid-column: 1;
+  grid-row: 1 / 4;
+
+  overflow: hidden;
+  border-radius: 20px;
 }
+
+.recipe-header .img-wrap img{
+  width: 100%;
+  transform: translateY(-20%);
+  border-radius: inherit;
+  padding-right: 10px;
+}
+
 .center {
   display: block;
   margin-left: auto;
   margin-right: auto;
   width: 50%;
 }
-/* .recipe-header{
 
-} */
+.span-decor {
+  color: darkblue;
+}
 </style>
