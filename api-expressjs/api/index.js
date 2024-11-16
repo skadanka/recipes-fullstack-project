6f9787bf0de6104471c0ba62f6c42d574c94d084
Eden Abuhatzeira <rylynn2@gmail.com>
2024-11-16 13:28:39 +0200

    prepering for deploy vercel

--- api-expressjs/api/index.js
+++ api-expressjs/api/index.js
@@ -1,13 +1,12 @@
 
 
 require("dotenv").config();
-var data = require('./recipe4000.json')
 //#region express configures
 var express = require("express");
 var path = require("path");
 var logger = require("morgan");
 const session = require("client-sessions");
-const DButils = require("./routes/utils/DButils");
+const DButils = require(.."/routes/utils/DButils");
 var cors = require('cors')
 
 var app = express();
@@ -45,11 +44,11 @@ const corsConfig = {
 app.use(cors(corsConfig));
 app.options("*", cors(corsConfig));
 
-var port = process.env.PORT || "3000"; //local=3000 remote=80
+// var port = process.env.PORT || "3000"; //local=3000 remote=80
 //#endregion
-const user = require("./routes/user");
-const recipes = require("./routes/recipes");
-const auth = require("./routes/auth");
+const user = require("../routes/user");
+const recipes = require("../routes/recipes");
+const auth = require("../routes/auth");
 
 
 //#region cookie middleware
@@ -72,46 +71,6 @@ app.use(function (req, res, next) {
 // ----> For cheking that our server is alive
 app.get("/alive", (req, res) => res.send("I'm alive"));
 
-// app.use(function (req, res, next) {
-//   res.send({
-//     recipes: [
-//     {
-//       id:0, 
-//       title: "Food example",
-//       readyInMinutes: 35.0,
-//       image: "https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=780&q=80",
-//       aggregateLikes: 100,
-//       glutenFree: true,
-//       isVegan: true,
-//       isVegetarian: false,
-//     },
-//     {
-//       id:1, 
-//       title: "Food example",
-//       readyInMinutes: 35.0,
-//       image: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=781&q=80",
-//       aggregateLikes: 100,
-//       glutenFree: true,
-//       isVegan: false,
-//       isVegetarian: true,
-
-//     },
-//     {
-//       id:2, 
-//       title: "Food example",
-//       readyInMinutes: 35.0,
-//       image: "https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80",
-//       aggregateLikes: 100,
-//       glutenFree: true,
-//       isVegan: true,
-//       isVegetarian: true,
-//     }
-//   ],
-//   recipeInfo: data
-
-// })
-//   return;
-// })
 
 // Routings
 app.use("/users", user);
@@ -125,14 +84,4 @@ app.use(function (err, req, res, next) {
 });
 
 
-
-const server = app.listen(port, () => {
-  console.log(`Server listen on port ${port}`);
-});
-
-process.on("SIGINT", function () {
-  if (server) {
-    server.close(() => console.log("server closed"));
-  }
-  process.exit();
-});
+module.exports = app;
---
+++ api-expressjs/dist/js/chunk-2d0e6711.f880fa30.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e6711"],{9983:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},l=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v(" Family Recipes Page ")])])}],r={},u=r,a=t("2877"),i=Object(a["a"])(u,c,l,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0e6711.f880fa30.js.map
